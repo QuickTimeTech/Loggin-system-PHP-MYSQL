ac5543b05c9b7c245cd0579986b95c03e930aaeb
Liam Baker <38255111+QuickTimeTech@users.noreply.github.com>
2024-08-31 22:03:18 +0100

    Add files via upload

---
+++ core/inc/heading.php
@@ -0,0 +1,19 @@
+<?php
+// Start the session
+session_start();
+
+// Check if the 'username' session variable is set
+if (!isset($_SESSION['username'])) {
+    // If not, redirect to the login page
+    header("Location: ../index.php");
+    // Exit the script
+    exit();
+}
+
+// Assign session variables to local variables
+$username = $_SESSION['username'];
+$email = $_SESSION['email'];
+$account_type = $_SESSION['account_type'];
+
+include '../config.php';
+?>
